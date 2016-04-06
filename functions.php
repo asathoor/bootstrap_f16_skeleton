f98fc6f26ca671f3b2ba08e2cc79b19ecb5e5b6e
asathoor <per@multimusen.dk>
2016-04-06 20:51:19 +0200

    dropped wp-nav-walker

--- functions.php
+++ functions.php
@@ -5,4 +5,5 @@ require_once('wp_bootstrap_navwalker.php');
 register_nav_menus( array(
     'primary' => __( 'Primary Menu', 'THEMENAME' ),
 ) );
+
 ?>
--- header.php
+++ header.php
@@ -19,40 +19,16 @@
 
 <!-- ends Bootstrap -->
 
+<!-- jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script>
+
 <!-- wp_head() -->
 <?php wp_head(); ?>
 
 </head>
 <body>
 
-<!-- bootstrap menu via nav-walker -->
- <nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>">
-                <?php bloginfo('name'); ?>
-            </a>
-    </div>
 
-        <?php
-            wp_nav_menu( array(
-                'menu'              => 'primary',
-                'theme_location'    => 'primary',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse',
-        'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
-            );
-        ?>
-    </div>
-</nav>
+
+
