ea39b823e00be926489aab0989150c39fa311d40
asathoor <per@multimusen.dk>
2016-04-06 19:51:25 +0200

    wp_head

--- header.php
+++ header.php
@@ -18,6 +18,10 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 <!-- ends Bootstrap -->
+
+<!-- wp_head() -->
+<?php wp_head(); ?>
+
 </head>
 <body>
 
