74bb58578af2ab12859b04d115473617802d80df
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2017-03-28 22:56:10 +0200

    Revert "Merge pull request #1 from asathoor/evening"
    This reverts commit 48e0b6d2507ed2cae40dabcd199a8579ca55a144.

--- sidebar.php
+++ sidebar.php
@@ -12,19 +12,9 @@
     <!-- Menu -->  
     <h3> <?php _e('Sidebar Menu','petj_f16_bootstrap'); ?> </h3>
 
-
-	    <?php wp_nav_menu( 
-				array( 
-
-					'theme_location' => 'sidebar-menu',
-					'container' => 'nav',
-					'container_id' => 'navSidebarMenu',
-					'container_class' => 'sidebarMenu',
-					'depth' => 0,
-					'items_wrap' => 'ul'
-
-				) ); ?>
-
+		<nav>
+	    <?php wp_nav_menu( array( 'theme_location' => 'sidebar-menu' ) ); ?>
+		</nav>
 
     <!-- widget areas -->
     <h3> <?php _e('Widget area','petj_f16_bootstrap'); ?> </h3>
