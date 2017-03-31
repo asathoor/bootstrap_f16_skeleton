e5a24832ba42f1a3286346c43e82d838941a11cf
asathoor <asathoor@gmail.com>
2017-03-31 17:55:16 +0200

    conflict in footer.php

--- footer.php
+++ footer.php
@@ -2,33 +2,8 @@
 
 	<!-- add your scripts here -->
 
-<<<<<<< Updated upstream
-<!-- Jquery menu sample --> 
-		  <script> 
-		    /* <![CDATA[ */ 
-		    ( function( $ ) { 
-		      // code goes here 
-		      console.log('Here\'s Jquery.'); 
-		      // hide the children 
-		      $('.children').toggle(); 
-		 
-		      // Based on @url: http://callmenick.com/post/slide-down-menu-with-jquery-and-css 
-		      $( '.page_item_has_children' ).hover( 
-		              function(){ 
-		                  $(this).children('.children').slideDown(500); 
-		              }, 
-		              function(){ 
-		                  $(this).children('.children').slideUp(500); 
-		              } 
-		          ); 
-		    } )( jQuery ); // jquery end 
-		    /* ]]> */ 
-		  </script> 
-
-
-
 	<?php wp_footer(); // load WP-related footer scripts ?>
-=======
+
 	<!-- Jquery menu sample --> 
 	<script> 
 		/* <![CDATA[ */ 
@@ -51,8 +26,7 @@
 		/* ]]> */ 
 	</script> 
 
-<?php wp_footer(); // load WP-related footer scripts ?>
+	<?php wp_footer(); // load WP-related footer scripts ?>
 
->>>>>>> Stashed changes
 </body>
 </html>
