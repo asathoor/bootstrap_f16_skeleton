ae6a6b1ad16d3acfed5a5fdbcc3e14868f152e58
asathoor <asathoor@gmail.com>
2017-03-31 17:49:06 +0200

    jq again

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 
 	<!-- add your scripts here -->
 
+<<<<<<< Updated upstream
 <!-- Jquery menu sample --> 
 		  <script> 
 		    /* <![CDATA[ */ 
@@ -27,5 +28,31 @@
 
 
 	<?php wp_footer(); // load WP-related footer scripts ?>
+=======
+	<!-- Jquery menu sample --> 
+	<script> 
+		/* <![CDATA[ */ 
+		( function( $ ) { 
+		  // code goes here 
+		  console.log('Here\'s Jquery.'); 
+		  // hide the children 
+		  $('.children').toggle(); 
+
+		  // Based on @url: http://callmenick.com/post/slide-down-menu-with-jquery-and-css 
+		  $( '.page_item_has_children' ).hover( 
+		          function(){ 
+		              $(this).children('.children').slideDown(500); 
+		          }, 
+		          function(){ 
+		              $(this).children('.children').slideUp(500); 
+		          } 
+		      ); 
+		} )( jQuery ); // jquery end 
+		/* ]]> */ 
+	</script> 
+
+<?php wp_footer(); // load WP-related footer scripts ?>
+
+>>>>>>> Stashed changes
 </body>
 </html>
