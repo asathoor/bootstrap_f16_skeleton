4e9ef6b820b89e44b40a9be6e0655f1cff216735
asathoor <asathoor@gmail.com>
2017-04-04 21:15:25 +0200

    pages
    about, cv

---
+++ page-about.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * file: single.php
+ */
+get_header();
+?>
+<!-- file: index.php -->
+
+<div class="container-fluid">
+
+		<aside class="col-sm-12 col-lg-4">
+		<!-- site description -->
+			<?php hej_verden( 
+				get_bloginfo('name'),
+				get_bloginfo('description')
+			); // see: functions.php ?>
+
+			<!-- avatar -->
+			<p><?php echo get_avatar(1,66); ?><br>
+			Editor: Per Thykjaer Jensen</p>
+				
+			<hr>			
+		</aside>
+
+    <main class="col-sm-12 col-lg-4">
+			<!-- loop start -->
+				<?php get_template_part( "loopPage" ); ?>
+			<!-- loop end -->
+    </main>
+
+    <aside class="col-sm-12 col-sm-4">
+			<?php petj_display_post( 1263, 'hand-right'); // CV ?>
+			<?php petj_display_post( 5347, 'hand-right'); // Dannebrog ?>
+			<?php petj_display_post( 5347, 'hand-right'); // Comp. CMS art. ?>
+			</hr>
+    </aside>
+
+  </div><!-- row -->
+</div><!-- .container -->
+
+<?php get_footer(); ?>
