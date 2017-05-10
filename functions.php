b9abdda6f9dfa36ce0ca1af68a681d2254abc7d0
Per Thykjaer Jensen <per@multimusen.dk>
2017-05-10 11:09:09 +0200

    google analytics

--- functions.php
+++ functions.php
@@ -21,6 +21,10 @@ function themeslug_enqueue_style() {
 
 	// bootstrap carousel
 	wp_enqueue_style( 'bootstrap-carousel', get_template_directory_uri() . '/partials/carousel.css', false );
+
+	// google analytics script
+	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/petjGanalytics.js', true );
+
 }
 add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_style' );
 
