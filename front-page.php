fc33287005acd96ea9422e2161996cf4a832f5ee
asathoor <asathoor@gmail.com>
2017-06-03 08:28:20 +0200

    logo in far away layer

--- front-page.php
+++ front-page.php
@@ -13,8 +13,8 @@
 		<?php wp_head(); ?>
 		
 		<!-- Behavioral Meta Data -->
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!-- meta name="apple-mobile-web-app-capable" content="yes" -->
+		<!-- meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" -->
 		
 		<style>
 			#container {
@@ -29,23 +29,11 @@
 	</head>
 
 		<body>
-		
-			<!-- markup inspired by @url: https://www.w3schools.com/bootstrap/ -->
-			<header class="jumbotron">
-			<a href="/">
-				<img src="<?php bloginfo('template_directory'); ?>/images/multimusen.png" 
-						alt="speaker" 
-						id="logoImage">
-		
-				<h1><?php bloginfo('name'); ?></h1>
-			</a>	
-			</header>
-
-		
-			<!-- Wagerfield's parallax -->
+					
+			<!-- Wagerfield's parallax layers -->
 			<div id="container" class="container">
 				<div id="scene" class="scene">
-					<div class="layer" data-depth="1.00"><img src="<?php bloginfo('template_directory'); ?>/images/layer1.png"></div>
+					<div class="layer" data-depth="1.00"><img src="<?php bloginfo('template_directory'); ?>/images/multimusen_plx.png"></div>
 					<div class="layer" data-depth="0.80"><img src="<?php bloginfo('template_directory'); ?>/images/layer2.png"></div>
 					<div class="layer" data-depth="0.60"><img src="<?php bloginfo('template_directory'); ?>/images/layer3.png"></div>
 					<div class="layer" data-depth="0.40"><img src="<?php bloginfo('template_directory'); ?>/images/layer4.png"></div>
@@ -53,12 +41,6 @@
 					<div class="layer" data-depth="0.00"><img src="<?php bloginfo('template_directory'); ?>/images/layer6.png"></div>
 				</div>
 			</div>
-			
-			<footer class="footer">
-			   <div class="container">
-			     <span class="text-muted">&copy; Multimusen.dk</span>
-			   </div>
-    		</footer>
 
 			<!-- Jquery -->
 			<?php wp_enqueue_script("jquery"); ?>
@@ -66,8 +48,8 @@
 			<!-- Scripts: Wagerfield's Parallax -->
 			<script src="<?php bloginfo('template_directory'); ?>/bower_components/parallax/deploy/parallax.js"></script>
 		
-			<script>	
-				// Pretty simple huh?
+			<script>
+				// enable parallax viewport	
 				var scene = document.getElementById('scene');
 				var parallax = new Parallax(scene);	
 			</script>
