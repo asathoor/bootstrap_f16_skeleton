8ba3814fd7087bc2ae69f024e3f4e08d4ea3d956
asathoor <asathoor@gmail.com>
2017-06-04 08:46:42 +0200

    head adjust for bootstrap
    as in template

--- front-page.php
+++ front-page.php
@@ -6,15 +6,28 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-	<head>
-	 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="HandheldFriendly" content="true">
+	<html lang="en">
+	  <head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	    	
+		<!-- Jquery -->
+		<?php wp_enqueue_script("jquery"); ?>
 		
+		<!-- hooking in WordPress -->
 		<?php wp_head(); ?>
-		
-	</head>
+	
+	
+	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	
+  </head>
 
 		<body>
 
@@ -91,8 +104,8 @@
 				</div><!-- /row -->
 			</div><!-- /container-fluid -->
 
-			<!-- Jquery -->
-			<?php wp_enqueue_script("jquery"); ?>
+
+			<?php wp_footer(); ?>
 
 			<!-- Scripts: Wagerfield's Parallax -->
 			<script src="<?php bloginfo('template_directory'); ?>/bower_components/parallax/deploy/parallax.js"></script>
@@ -105,10 +118,6 @@
 
 			<!-- Google Analytics -->
 			<script src="<?php bloginfo('template_directory'); ?>/js/petjGanalytics.js"></script>
-
-
-			<?php wp_footer(); ?>
-			
 			
 		</body>
 </html>
