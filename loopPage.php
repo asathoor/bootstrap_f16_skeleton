83ce76df1882fee5e9ba418eff7c6481d3331c78
asathoor <asathoor@gmail.com>
2017-06-06 21:43:42 +0200

    looppage

--- loopPage.php
+++ loopPage.php
@@ -4,44 +4,24 @@
  * See WP loop page, @url https://codex.wordpress.org/The_Loop
  */
 ?>
-<!-- Start the Loop. @url: https://codex.wordpress.org/The_Loop -->
+<!-- file: loopPage.php -->
  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
  
  	<div class="post">
+		<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 
- 	<!-- Display the Title as a link to the Post's permalink. -->
+		<div id="date"><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></div>
+	
+	
+	 	<div class="entry">
+	 		<?php // the_content(); ?>
+	 	</div>
 
- 	<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
-
- 	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
-
- 	<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
-
-
- 	<!-- Display the Post's content in a div box. -->
-
- 	<div class="entry">
- 		<?php the_content(); ?>
- 	</div>
-
-
- 	<!-- Display a comma separated list of the Post's Categories. -->
-
-	<hr>
  	</div> <!-- closes the first div box -->
-
-
- 	<!-- Stop The Loop (but note the "else:" - see next line). -->
 
  <?php endwhile; else : ?>
 
+ 	<p><?php _e( 'I\'ve got nothing to display ...' ); ?></p>
 
- 	<!-- The very first "if" tested to see if there were any Posts to -->
- 	<!-- display.  This "else" part tells what do if there weren't any. -->
- 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-
-
- 	<!-- REALLY stop The Loop. -->
  <?php endif; ?>
+<!-- end: loopPage.php -->
